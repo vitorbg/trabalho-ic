555ccbb6bc15495b4f683536ba07f58d8c563a69
Vitor Gonçalves <vitorbg92@gmail.com>
2017-05-02 23:48:35 -0300

    Adição de função fitness

--- ag/domain/individuo.go
+++
@@ -1,6 +0,0 @@
-package domain
-
-type Individuo struct {
-	Gene [8]int
-  Nota int
-}
--- ag/main.go
+++ ag/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	//"github.com/vitorbg/trabalho-ic/ag/domain"
+	//"math"
 	"math/rand"
 	"time"
 )
@@ -11,17 +11,29 @@ const POPULATION_SIZE = 10
 const INDIVIDUAL_SIZE = 12
 const POS_FITNESS = 10
 
+const (
+	D = iota // 0
+	E        // 1
+	M        // 2
+	N
+	O
+	R
+	S
+	Y
+)
 
-var currentPopulation [POPULATION_SIZE][11]int
-var nextPopulation [POPULATION_SIZE][11]int
+var currentPopulation [POPULATION_SIZE][INDIVIDUAL_SIZE]int
+var nextPopulation [POPULATION_SIZE][INDIVIDUAL_SIZE]int
 
 func main() {
 
 	fmt.Println("Galoo")
 	initPopulation()
 	printPopulation()
-
-
+	fmt.Println("Olha o fitnes")
+	calcIndividualFitness(&currentPopulation[0])
+	calcPopulationFitness()
+	printPopulation()
 }
 
 func initPopulation() {
@@ -51,4 +63,55 @@ func printPopulation() {
 		fmt.Println("Individuo ", i)
 		fmt.Println(currentPopulation[i])
 	}
+}
+
+func calcPopulationFitness() {
+
+	for i := 0; i < POPULATION_SIZE; i++ {
+		for j := 0; j < INDIVIDUAL_SIZE; j++ {
+			calcIndividualFitness(&currentPopulation[i])
+		}
+	}
+
+}
+
+func calcIndividualFitness(individual *[INDIVIDUAL_SIZE]int) {
+
+	var send, more, money int
+	send += individual[S] * 1000
+	send += individual[E] * 100
+	send += individual[N] * 10
+	send += individual[D]
+
+	more += individual[M] * 1000
+	more += individual[O] * 100
+	more += individual[R] * 10
+	more += individual[E]
+
+	money += individual[M] * 10000
+	money += individual[O] * 1000
+	money += individual[N] * 100
+	money += individual[E] * 10
+	money += individual[Y]
+
+	fitness := (send + more) - money
+	if fitness < 0 {
+		fitness = fitness * (-1)
+	}
+	individual[10] = fitness
+
+	fmt.Println(individual)
+	fmt.Println("SEND: ", send)
+	fmt.Println("MORE: ", more)
+	fmt.Println("MONEY: ", money)
+	fmt.Println("SEND+MORE: ", send+more)
+	fmt.Println("(SEND+MORE)-MONEY: ", fitness)
+	fmt.Println("D:", individual[D])
+	fmt.Println("E:", individual[E])
+	fmt.Println("M:", individual[M])
+	fmt.Println("N:", individual[N])
+	fmt.Println("O:", individual[O])
+	fmt.Println("R:", individual[R])
+	fmt.Println("S:", individual[S])
+	fmt.Println("Y:", individual[Y])
 }
